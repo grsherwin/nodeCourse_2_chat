367fda81b33f96699a127943167a1f7ba17a384e
Glen Sherwin <glensherwin@Glens-MacBook-Pro.local>
2018-11-23 21:54:18 -0600

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,4 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
-})
+});
--- server/server.js
+++ server/server.js
@@ -24,13 +24,30 @@ io.on('connection', (socket) => {
   //   createdAt: 123123
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'A new user joined',
+      createdAt: new Date().getTime()
+    });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      ceatedAt: new Date().getTime()
+      createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     ceatedAt: new Date().getTime()
+    //   });
   });
 
   socket.on('disconnect', () => {
